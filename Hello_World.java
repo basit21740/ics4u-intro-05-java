16acc602aee1fa4c56fc0987754696ccbfad2f8f
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-02-10 17:43:28 +0000

    added

---
+++ Hello_World.java
@@ -0,0 +1,10 @@
+// Java file practice on ssh
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+	System.out.println("");
+	System.out.println("Done.");
+    }
+}
+
